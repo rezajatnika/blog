68ed256c2bbc499b11cc40e5fcadb46aee0441a3
Reza Jatnika <rezajatnika@gmail.com>
2014-04-23 02:09:43 +0700

    Refactor articles path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Root application path
-  root 'welcome#index'
+  root 'articles#index'
 
   # Article resources for Article CRUD operations
   resources :articles
